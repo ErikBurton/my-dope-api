6f16bee5254e1929d4a319ed9d1ee66846ad8d78
Erik Burton <erikburton1@gmail.com>
2022-02-01 20:22:47 -0500

    Adds product info to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create([
+    {
+      name: "Quiet Comfort 35",
+      brand: "Bose",
+      price: "$279.99",
+      description: "Wireless Bluetooth Headphones"
+    },
+    {
+      name: "iPhone 13",
+      brand: "Apple",
+      price: "$1279.99",
+      description: "Latest phone from Apple"
+    },
+    {
+      name: "Camaro ZL1 1/18th Die Cast Car",
+      brand: "Acme",
+      price: "$329.99",
+      description: "1969 Fred Gibb/Dick Harell ZL1 Camaro"
+    },
+    {
+      name: "22 Bass Drum Head",
+      brand: "Remo",
+      price: "$49.99",
+      description: "22 inch ebony bass drum head"
+    }
+    ])
+  
